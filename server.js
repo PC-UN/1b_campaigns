7c010606dd553fdbb2a54427a6d636ed4db4d537
Jairo Suárez <jaasuarezga@gmail.com>
2018-05-16 10:45:12 -0500

    Change host invalid character

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express'),
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/campaigns_db');
-mongoose.connect('mongodb://campaigns_db');
+mongoose.connect('mongodb://campaigns-db');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
